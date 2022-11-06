760c79fb20f0c0347996715db99877002125fe59
Ankita Gonnade <52381158+ankitagonnade@users.noreply.github.com>
2022-11-06 11:12:24 -0600

    Add files via upload

---
+++ Face Recognition and Object Detection/Facial Recognition/screenshots/recognize_faces_video.py
@@ -0,0 +1,224 @@
+# USAGE
+# python recognize_faces_video.py --encodings encodings.pickle
+# python recognize_faces_video.py --encodings encodings.pickle --output output/jurassic_park_trailer_output.avi --display 0 --prototxt MobileNetSSD_deploy.prototxt.txt --model MobileNetSSD_deploy.caffemodel
+
+# import the necessary packages
+from imutils.video import VideoStream
+import numpy as np
+import face_recognition
+import argparse
+import imutils
+import pickle
+import time
+import cv2
+
+# construct the argument parser and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-e", "--encodings", required=True,
+	help="path to serialized db of facial encodings")
+ap.add_argument("-o", "--output", type=str,
+	help="path to output video")
+ap.add_argument("-y", "--display", type=int, default=1,
+	help="whether or not to display output frame to screen")
+ap.add_argument("-p", "--prototxt", required=True,
+	help="path to Caffe 'deploy' prototxt file")
+ap.add_argument("-m", "--model", required=True,
+	help="path to Caffe pre-trained model")
+ap.add_argument("--thr", default=0.4, type=float, help="confidence threshold to filter out weak detections")
+ap.add_argument("-d", "--detection-method", type=str, default="cnn",
+	help="face detection model to use: either `hog` or `cnn`")
+args = vars(ap.parse_args())
+
+# initialize the list of class labels MobileNet SSD was trained to
+# detect, then generate a set of bounding box colors for each class
+#CLASSES = ["background", "aeroplane", "bicycle", "bird", "boat",
+#	"bottle", "bus", "car", "cat", "chair", "cow", "diningtable",
+#	"dog", "horse", "motorbike", "pottedplant", "sheep",
+#	"sofa", "train", "tvmonitor", "book"]
+CLASSES = { 0: 'background',
+            1: 'person', 2: 'bicycle', 3: 'car', 4: 'motorcycle', 5: 'airplane', 6: 'bus',
+            7: 'train', 8: 'truck', 9: 'boat', 10: 'traffic light', 11: 'fire hydrant',
+            13: 'stop sign', 14: 'parking meter', 15: 'bench', 16: 'bird', 17: 'cat',
+            18: 'dog', 19: 'horse', 20: 'sheep', 21: 'cow', 22: 'elephant', 23: 'bear',
+            24: 'zebra', 25: 'giraffe', 27: 'backpack', 28: 'umbrella', 31: 'handbag',
+            32: 'tie', 33: 'suitcase', 34: 'frisbee', 35: 'skis', 36: 'snowboard',
+            37: 'sports ball', 38: 'kite', 39: 'baseball bat', 40: 'baseball glove',
+            41: 'skateboard', 42: 'surfboard', 43: 'tennis racket', 44: 'bottle',
+            46: 'wine glass', 47: 'cup', 48: 'fork', 49: 'knife', 50: 'spoon',
+            51: 'bowl', 52: 'banana', 53: 'apple', 54: 'sandwich', 55: 'orange',
+            56: 'broccoli', 57: 'carrot', 58: 'hot dog', 59: 'pizza', 60: 'donut',
+            61: 'cake', 62: 'chair', 63: 'couch', 64: 'potted plant', 65: 'bed',
+            67: 'dining table', 70: 'toilet', 72: 'tv', 73: 'laptop', 74: 'mouse',
+            75: 'remote', 76: 'keyboard', 77: 'cell phone', 78: 'microwave', 79: 'oven',
+            80: 'toaster', 81: 'sink', 82: 'refrigerator', 84: 'book', 85: 'clock',
+            86: 'vase', 87: 'scissors', 88: 'teddy bear', 89: 'hair drier', 90: 'toothbrush' }
+
+IGNORE = set(["person"])
+COLORS = np.random.uniform(0, 255, size=(len(CLASSES), 3))
+
+inWidth = 300
+inHeight = 300
+WHRatio = inWidth / float(inHeight)
+inScaleFactor = 0.007843
+meanVal = 127.5
+swapRB = True
+# load our serialized model from disk
+print("[INFO] loading model...")
+#net = cv2.dnn.readNetFromCaffe(args["prototxt"], args["model"])
+net = cv2.dnn.readNetFromTensorflow(args["model"], args["prototxt"])
+
+# load the known faces and embeddings
+print("[INFO] loading encodings...")
+data = pickle.loads(open(args["encodings"], "rb").read())
+
+# initialize the video stream and pointer to output video file, then
+# allow the camera sensor to warm up
+print("[INFO] starting video stream...")
+vs = VideoStream(src=0).start()
+#cap=cv2.VideoCapture('http://10.42.0.152:4747/video')
+writer = None
+time.sleep(2.0)
+
+# loop over frames from the video file stream
+while True:
+	# grab the frame from the threaded video stream
+	frame = vs.read()
+	
+	# convert the input frame from BGR to RGB then resize it to have
+	# a width of 750px (to speedup processing)
+	rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+	rgb = imutils.resize(frame, width=750)
+	r = frame.shape[1] / float(rgb.shape[1])
+
+	# detect the (x, y)-coordinates of the bounding boxes
+	# corresponding to each face in the input frame, then compute
+	# the facial embeddings for each face
+	boxes = face_recognition.face_locations(rgb,
+		model=args["detection_method"])
+	encodings = face_recognition.face_encodings(rgb, boxes)
+	names = []
+
+	# loop over the facial embeddings
+	for encoding in encodings:
+		# attempt to match each face in the input image to our known
+		# encodings
+		matches = face_recognition.compare_faces(data["encodings"],
+			encoding, tolerance=0.5)
+		name = "Unknown"
+
+		# check to see if we have found a match
+		if True in matches:
+			# find the indexes of all matched faces then initialize a
+			# dictionary to count the total number of times each face
+			# was matched
+			matchedIdxs = [i for (i, b) in enumerate(matches) if b]
+			counts = {}
+
+			# loop over the matched indexes and maintain a count for
+			# each recognized face face
+			for i in matchedIdxs:
+				name = data["names"][i]
+				counts[name] = counts.get(name, 0) + 1
+
+			# determine the recognized face with the largest number
+			# of votes (note: in the event of an unlikely tie Python
+			# will select first entry in the dictionary)
+			name = max(counts, key=counts.get)
+		
+		# update the list of names
+		names.append(name)
+
+	# loop over the recognized faces
+	for ((top, right, bottom, left), name) in zip(boxes, names):
+		# rescale the face coordinates
+		top = int(top * r)
+		right = int(right * r)
+		bottom = int(bottom * r)
+		left = int(left * r)
+
+		# draw the predicted face name on the image
+		cv2.rectangle(frame, (left, top), (right, bottom),
+			(0, 255, 0), 2)
+		y = top - 15 if top - 15 > 15 else top + 15
+		cv2.putText(frame, name, (left, y), cv2.FONT_HERSHEY_SIMPLEX,
+			0.75, (0, 255, 0), 2)
+
+	# grab the frame dimensions and convert it to a blob
+	blob = cv2.dnn.blobFromImage(frame, inScaleFactor, (inWidth, inHeight), (meanVal, meanVal, meanVal), swapRB)
+
+	# pass the blob through the network and obtain the detections and
+	# predictions
+	net.setInput(blob)
+	detections = net.forward()
+
+	cols = frame.shape[1]
+        rows = frame.shape[0]
+
+        if cols / float(rows) > WHRatio:
+            cropSize = (int(rows * WHRatio), rows)
+        else:
+            cropSize = (cols, int(cols / WHRatio))
+
+        y1 = int((rows - cropSize[1]) / 2)
+        y2 = y1 + cropSize[1]
+        x1 = int((cols - cropSize[0]) / 2)
+        x2 = x1 + cropSize[0]
+        frame = frame[y1:y2, x1:x2]
+
+        cols = frame.shape[1]
+        rows = frame.shape[0]
+
+        for i in range(detections.shape[2]):
+            confidence = detections[0, 0, i, 2]
+            if confidence > args["thr"]:
+                class_id = int(detections[0, 0, i, 1])
+
+                xLeftBottom = int(detections[0, 0, i, 3] * cols)
+                yLeftBottom = int(detections[0, 0, i, 4] * rows)
+                xRightTop   = int(detections[0, 0, i, 5] * cols)
+                yRightTop   = int(detections[0, 0, i, 6] * rows)
+
+                cv2.rectangle(frame, (xLeftBottom, yLeftBottom), (xRightTop, yRightTop),
+                              (0, 255, 0))
+		if CLASSES[class_id] in IGNORE:
+				continue
+                if class_id in CLASSES:
+                    label = CLASSES[class_id] + ": " + str(confidence)
+                    labelSize, baseLine = cv2.getTextSize(label, cv2.FONT_HERSHEY_SIMPLEX, 0.5, 1)
+
+                    yLeftBottom = max(yLeftBottom, labelSize[1])
+                    cv2.rectangle(frame, (xLeftBottom, yLeftBottom - labelSize[1]),
+                                         (xLeftBottom + labelSize[0], yLeftBottom + baseLine),
+                                         (255, 255, 255), cv2.FILLED)
+                    cv2.putText(frame, label, (xLeftBottom, yLeftBottom),
+                                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0))
+
+	# if the video writer is None *AND* we are supposed to write
+	# the output video to disk initialize the writer
+	if writer is None and args["output"] is not None:
+		fourcc = cv2.VideoWriter_fourcc(*"MJPG")
+		writer = cv2.VideoWriter(args["output"], fourcc, 20,
+			(frame.shape[1], frame.shape[0]), True)
+
+	# if the writer is not None, write the frame with recognized
+	# faces t odisk
+	if writer is not None:
+		writer.write(frame)
+
+	# check to see if we are supposed to display the output frame to
+	# the screen
+	if args["display"] > 0:
+		cv2.imshow("Frame", frame)
+		key = cv2.waitKey(1) & 0xFF
+
+		# if the `q` key was pressed, break from the loop
+		if key == ord("q"):
+			break
+
+# do a bit of cleanup
+cv2.destroyAllWindows()
+vs.stop()
+
+# check to see if the video writer point needs to be released
+if writer is not None:
+	writer.release()
